b5888d3d31b2caa5a912cf64d85fb488615bb191
Altay Gençaslan <altay.gencaslan@ext.kocdigital.com>
2024-10-16 12:15:41 +0300

    feat: Case resolved

--- Integration/Service/ItemIntegrationService.cs
+++ Integration/Service/ItemIntegrationService.cs
@@ -1,5 +1,7 @@
 ﻿using Integration.Common;
 using Integration.Backend;
+using System.Runtime.Caching;
+using StackExchange.Redis;
 
 namespace Integration.Service;
 
@@ -8,21 +10,37 @@ public sealed class ItemIntegrationService
     //This is a dependency that is normally fulfilled externally.
     private ItemOperationBackend ItemIntegrationBackend { get; set; } = new();
 
+    private static readonly MemoryCache _cache = MemoryCache.Default;
+    private object lockItem = new();
+
     // This is called externally and can be called multithreaded, in parallel.
     // More than one item with the same content should not be saved. However,
     // calling this with different contents at the same time is OK, and should
     // be allowed for performance reasons.
-    public Result SaveItem(string itemContent)
+    public async Task<Result> SaveItem(string itemContent)
     {
-        // Check the backend to see if the content is already saved.
-        if (ItemIntegrationBackend.FindItemsWithContent(itemContent).Count != 0)
+        return
+        await Task<Result>.Run(() =>
         {
-            return new Result(false, $"Duplicate item received with content {itemContent}.");
-        }
+            lock (lockItem)
+            {
+                bool existInCache = _cache.Contains(itemContent);
+                // Check the backend to see if the content is already saved.
+                if (existInCache || ItemIntegrationBackend.FindItemsWithContent(itemContent).Count != 0)
+                {
+                    return new Result(false, $"Duplicate item received with content {itemContent}.");
+                }
 
-        var item = ItemIntegrationBackend.SaveItem(itemContent);
+                //Date bilgisi ihtiyaç halinde değişir,
+                //Şimdilik db'ye kayıt süresi + 5 saniye olarak set'lendi
+                //ancak optimum süreye değiştirilmelidir
+                _cache.Add(itemContent, true, DateTime.Now.AddSeconds(45));
+            }
 
-        return new Result(true, $"Item with content {itemContent} saved with id {item.Id}");
+            var item = ItemIntegrationBackend.SaveItem(itemContent);
+            return new Result(true, $"Item with content {itemContent} saved with id {item.Id}");
+        });
+
     }
 
     public List<Item> GetAllItems()
